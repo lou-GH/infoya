a283a0abd71a50b2ecdad8ee9756865e88c12d7d
lou-GH <117365073+lou-GH@users.noreply.github.com>
2023-04-13 12:15:15 +0000

    デプロイ前準備5

--- db/migrate/20230307142420_create_posts.rb
+++ db/migrate/20230307142420_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
 
-      t.references :manufacturer, foreign_key: true
+      t.references :manufacturer, type: :bigint, foreign_key: true
       t.references :location, foreign_key: true
       t.references :genre_tag, foreign_key: true
       t.references :notification, foreign_key: true
