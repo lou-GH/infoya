4bf87a16098bada197371a6a23652a2a9ff34845
lou-GH <117365073+lou-GH@users.noreply.github.com>
2023-04-13 12:10:55 +0000

    デプロイ前準備4

--- db/migrate/20230216133451_devise_create_manufacturers.rb
+++ db/migrate/20230216133451_devise_create_manufacturers.rb
@@ -32,7 +32,7 @@ class DeviseCreateManufacturers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.references :location, foreign_key: true
+      # t.references :location, foreign_key: true
 
       t.string :account_name, null: false, default: ""
       t.string :introduction, default: ""
